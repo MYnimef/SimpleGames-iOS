9464410245005f08baa22b7ede803eb4f83d212b
MYnimef <ivan.markov.2013@gmsil.com>
2022-04-03 22:05:07 +0300

    made rps game

---
+++ SimpleGames/GameResult.swift
@@ -0,0 +1,13 @@
+//
+//  GameResult.swift
+//  SimpleGames
+//
+//  Created by Ivan Markov on 03.04.2022.
+//
+
+enum GameResult: String {
+    
+    case Human = "Вы победили"
+    case Computer = "Вы проиграли"
+    case NoOne = "Ничья"
+}
